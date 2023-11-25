200c3dfac2833ef9e3c78b7ec499d92d274fdb16
tinyynoob <sadpotato1920@gmail.com>
2023-11-26 02:53:24 +0800

    Add leetcode 1685 naive

--- src/main.rs
+++ src/main.rs
@@ -52,4 +52,19 @@ impl dyn Solution {
             max = second_max;
         }
     }
+    pub fn get_sum_absolute_differences(nums: Vec<i32>) -> Vec<i32> {
+        let mut ans: Vec<i32> = Vec::new();
+        let sum: i32 = nums.iter().sum();
+        let n = nums.len() as i32;
+        for (i, val) in nums.iter().enumerate() {
+            if i == 0 {
+                ans.push(sum - val * n);
+            } else {
+                let last = ans[i - 1];
+                let d = nums[i] - nums[i - 1];  // val - nums[i-1]
+                ans.push(last + (2 * (i as i32) - n) * d);
+            }
+        }
+        ans
+    }
 }
